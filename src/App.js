fdfa908d0b1b5330a6d025f368a9d84ab51d19f9
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-03-14 13:03:22 +0800

    cv-scanner initial setup for frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({
+    first_name: "",
+    last_name: "",
+  });
+  const [fileData, setFileData] = useState(null);
+
+  const handleFileChange = (e) => {
+    let file = e.target.files[0];
+    var fileData1 = new FormData();
+    fileData1.append("file", file);
+    setFileData(fileData1);
+  };
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+    axios
+      .post(
+        `http://localhost:8080/api/register?user=${encodeURIComponent(
+          JSON.stringify(data)
+        )}`,
+        fileData
+      )
+      .then((res) => {
+        console.log(res, "the result");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>First Name</label>
+        <br></br>
+        <input
+          type="text"
+          name="first_name"
+          value={data.first_name}
+          onChange={handleChange}
+        />
+        <br></br>
+        <label>Last Name</label>
+        <br></br>
+        <input
+          type="text"
+          name="last_name"
+          value={data.last_name}
+          onChange={handleChange}
+        />
+        <br></br>
+        <label>Upload resume / CV</label>
+        <br></br>
+        <input type="file" name="file" onChange={handleFileChange} />
+        <br></br>
+        <input type="submit" value="Submit" />
+        <br></br>
+      </form>
     </div>
   );
 }
