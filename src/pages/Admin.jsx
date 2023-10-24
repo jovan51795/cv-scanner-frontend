1433bb507ad52a94b4475286d128bce41a6e9c5a
zione8036 <74102490+zione8036@users.noreply.github.com>
2023-10-24 11:47:12 +0800

    implement pagination in the table

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import { env } from "../env";
 import { getAllKeywords } from "../services/cv_tagging";
 import { http } from "../services/http";
-import { Button, TextField, Card, CardContent } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 
 const Admin = () => {
   const [keyWord, setKeyWord] = useState("");
   const [words, setWords] = useState([]);
   const [loading, setLoading] = useState(true);
   const [showNoData, setShowNoData] = useState(false);
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +27,7 @@ const Admin = () => {
   };
 
   const getKeyWords = async () => {
-    getAllKeywords().then((res) => {
+    getAllKeywords(page, pageSize).then((res) => {
       console.log(res.data);
       setWords(res.data);
       setLoading(false);
@@ -37,8 +40,28 @@ const Admin = () => {
       if (words.length === 0) {
         setShowNoData(true);
       }
-    }, 10000); // 10 seconds
-  }, []);
+    }, 10000);
+  }, [page, pageSize]);
+  const increasePage = () => {
+    setPage((prevPage) => prevPage + 1);
+    console.log(page);
+    getKeyWords();
+  };
+
+  const decreasePage = () => {
+    if (page > 0) {
+      setPage((prevPage) => prevPage - 1);
+      console.log(page);
+      getKeyWords();
+    }
+  };
+
+  const handlePageSizeChange = (event) => {
+    const newPageSize = parseInt(event.target.value);
+    if (!isNaN(newPageSize) && newPageSize > 0) {
+      setPageSize(newPageSize);
+    }
+  };
 
   const columns = [
     {
@@ -50,6 +73,12 @@ const Admin = () => {
     {
       field: "keyword",
       headerName: "Keyword",
+      width: 400,
+      headerAlign: "center",
+    },
+    {
+      field: "action",
+      headerName: "Actions",
       width: 400,
       headerAlign: "center",
     },
@@ -77,7 +106,39 @@ const Admin = () => {
             "No Data"
           ) : null
         ) : (
-          <DataGrid rows={words} columns={columns} pageSize={5} />
+          <>
+            <DataGrid rows={words} columns={columns} pageSize={5} hideFooter />
+            <div
+              style={{
+                marginTop: "20px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div style={{ marginRight: "10px" }}>
+                <label>Page Size:</label>
+                <TextField
+                  type="number"
+                  value={pageSize}
+                  onChange={handlePageSizeChange}
+                  size="small"
+                />
+              </div>
+              <div>
+                <Button
+                  onClick={decreasePage}
+                  variant="outlined"
+                  disabled={page === 0}
+                >
+                  <KeyboardArrowLeft />
+                </Button>
+                <Button onClick={increasePage} variant="outlined">
+                  <KeyboardArrowRight />
+                </Button>
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
